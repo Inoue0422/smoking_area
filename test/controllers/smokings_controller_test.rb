2d4fb7ee5cd6e5e6c95677ada7c4608c66e188e9
Inoue0422 <o9045628081@gmail.com>
2023-12-04 18:23:53 +0900

    rubocop実行

--- test/controllers/smokings_controller_test.rb
+++ test/controllers/smokings_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class SmokingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get smokings_index_url
     assert_response :success
   end
