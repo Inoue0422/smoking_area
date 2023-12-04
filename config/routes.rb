3b72a6916a9730299c201db54ce268b703edb208
Inoue0422 <o9045628081@gmail.com>
2023-12-04 18:18:42 +0900

    userモデル単体テストコード

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'spots/index'
   root 'spots#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
