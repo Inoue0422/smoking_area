9bbb930751b4a51bdc02ee7ad0d8df7e59733b6b
Inoue0422 <o9045628081@gmail.com>
2024-01-13 22:28:56 +0900

    spotモデル単体テストコード(成功時)

--- spec/factories/spots.rb
+++ spec/factories/spots.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
   factory :spot do
+    name             { Faker::Name.name }
+    tobacco_id       { Faker::Number.between(from: 1, to: 3) }
+    fence            { [true, false].sample }
+    chair            { [true, false].sample }
+    roof             { [true, false].sample }
+    latitude         { Faker::Address.latitude }
+    longitude        { Faker::Address.longitude }
   end
 end
--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @spot = FactoryBot.build(:spot)
+  end
+
+  describe '喫煙所新規登録' do
+    context '喫煙所新規登路できる場合' do
+      it '喫煙所名前とタバコの種類と囲いと屋根と椅子と緯度と経度が存在すれば登録できる' do
+        expect(@spot).to be_valid
+      end
+      it '囲いと屋根と椅子がなくても登録できる' do
+        @spot.fence = ''
+        @spot.roof = ''
+        @spot.chair = ''
+        expect(@spot).to be_valid
+      end
+    end
+  end
 end
